4dc5101067a5a6de9527563c4fab806f3a7e19e6
Samir Erraji <Samir.Erraji@gmail.com>
2012-01-17 22:10:45 +0100

    Should work when we find how to configure the correct environment

---
+++ scrapper/app.py
@@ -0,0 +1,11 @@
+#We want to developpe a scrapper app.
+#It should start with a getting the content of a given url. 
+# Nothing agains using a few helper libraries.
+import urllib2
+
+file = urllib2.urlopen('http://www.google.com')
+raw = file.read()
+
+from BeautifulSoup import BeautifulSoup
+b = BeautifulSoup()
+b.prettefy(raw)
