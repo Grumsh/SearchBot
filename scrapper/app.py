b9d1691487e51e328ecffe930715009ed01d3695
Samir Erraji <Samir.Erraji@gmail.com>
2012-01-22 13:05:04 +0100

    Added the depth component and a set to skip already requested urls.

--- scrapper/app.py
+++ scrapper/app.py
@@ -1,22 +1,48 @@
 #We want to developpe a scrapper app.
 
-url = 'http://www.hackernews.com'
+url = 'http://news.ycombinator.com'
+pattern = 'sopa'
 #It should start with a getting the content of a given url. 
-import urllib2
-file = urllib2.urlopen(url)
-raw = file.read()
-
-print ('downloaded file from url '+url+' content was '+str(len(raw))+' bytes')
-
-#Multiple ways to extract the links
-#either using beautiful soup.
-from BeautifulSoup import BeautifulSoup
-b = BeautifulSoup(raw)
-
-#or using lxml. 
+import urllib2 
 from lxml import etree
 import StringIO
-parser = etree.HTMLParser()
-tree   = etree.parse(StringIO.StringIO(raw), parser)
-links = tree.xpath('//a')
-print ('found '+str(len(links))+' links in the file')
+import re
+
+seenUrls = set()
+
+def scrape (url, depth):
+	if (depth == 0):
+		return
+	if(url.find("http") != 0):	
+		return
+	if(url in seenUrls):
+		#print ('skipping '+url)
+		return
+	#print (depth * '*')
+	seenUrls.add(url)
+	#print (str(depth) + ': ' + url)
+	try:
+		file = urllib2.urlopen(url)
+		raw = file.read()
+		if(len(raw) == 0):
+		  return;
+
+		#Multiple ways to extract the links
+		#either using beautiful soup.
+		#or using lxml.
+		parser = etree.HTMLParser()
+		tree   = etree.parse(StringIO.StringIO(raw), parser)
+		textContent = tree.xpath('string()')
+		if(re.match(pattern, textContent, re.IGNORECASE)):
+			print ('match for '+url)
+		#We filter anchor that do not have a destination
+		links = tree.xpath('//a[@href]')
+		for link in links:
+			if('href' in link.attrib):
+				scrape(link.attrib['href'], depth -1)
+	except urllib2.URLError:
+		pass
+	except:
+		raise
+
+scrape(url, 5)
